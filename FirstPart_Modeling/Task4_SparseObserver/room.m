d524726b06f670c4a5f0d6ea2d651b09d03a2a1d
FedePreto <73708523+FedePreto@users.noreply.github.com>
2024-04-20 19:51:16 +0200

    Update room.m
    cambiata la logica di posizionamento dei sensori, ora anche se la cella ha due o più sensori è possibile distinguere quale è quello sotto attacco

--- FirstPart_Modeling/Task4_SparseObserver/room.m
+++ FirstPart_Modeling/Task4_SparseObserver/room.m
@@ -13,30 +13,34 @@ function room(x_estimated,a_estimated,Tstart,Tmax)
     W = 100; %width of a square cell (cm)
 
     room_grid = zeros(2,n);
-    count=0;
-    %sensor localization
-    sensors_position=zeros(q,2);%vector with the position of sensors and the number of sensors in the same cell
-    sensors=zeros(q,1); %vector with the position of sensors (repeated)
-    for i=1:q
-      [~,cell]=maxk(D(i,:),1);
-      sensors(i)=cell;
-      if ismember(cell,sensors_position(:,1))
-        pos=find(sensors_position(:,1)==cell);
-        sensors_position(pos,2)=sensors_position(pos,2)+1;
-      else
-        count=count+1;
-        sensors_position(i,1)=cell;
-        sensors_position(i,2)=1;           
-      end  
-    end
-    sensors_position(sensors_position==0)=[];
-    sensors_position = reshape(sensors_position,count,2);
-        
-  
-
     for i=1:n
 	    room_grid(1,i) = floor(W/2)+ mod(i-1,L)*W; 
 	    room_grid(2,i) = floor(W/2)+ floor((i-1)/L)*W;
+    end
+    count=0;
+    %% sensors localization
+    %sensors_position=zeros(q,2);%vector with the position of sensors and the number of sensors in the same cell
+    sensors=zeros(q,3); %vector with the position of sensors (repeated)
+    for i=1:q
+      [~,cell]=maxk(D(i,:),1);
+      sensors(i,1)=cell;
+    end
+    for i=1:q
+        index = find(sensors==sensors(i,1));
+        switch size(index,1)
+            case 1
+                sensors(i,2) = room_grid(1,sensors(i,1));
+                sensors(i,3) = room_grid(2,sensors(i,1));
+            case 2
+                sensors(index(1),2) = room_grid(1,sensors(index(1),1))-25;
+                sensors(index(1),3) = room_grid(2,sensors(index(1),1));
+                sensors(i,2) = room_grid(1,sensors(i,1))+25;
+                sensors(i,3) = room_grid(2,sensors(i,1));
+            case 3
+                %TODO
+            case 4
+                %TODO
+        end
     end
     
     %The definition of initial condition on x_true and a_true are given only for graphical
@@ -57,25 +61,19 @@ function room(x_estimated,a_estimated,Tstart,Tmax)
         target_estimated = find(x_estimated(:,move));
 
         sensors_under_attack = sensors(support_a);
-        estimated_attack = sensors(find(a_estimated(:,move)));
+        support_attack = find(a_estimated(:,move));
+        estimated_attack=zeros(size(support_attack,1),2);
+        for i = 1:size(support_attack,1)
+            estimated_attack(i,:) = sensors(support_attack(i),2:end);
+        end
         
         plot(room_grid(1,target_real), room_grid(2,target_real),'square','MarkerSize',9, 'MarkerEdgeColor',1/255*[40 208 220],'MarkerFaceColor',1/255*[40 208 220]);
         hold on
         plot(room_grid(1,target_estimated), room_grid(2,target_estimated),'*','MarkerSize',9, 'MarkerEdgeColor',1/255*[28 55 189]);
-        for i=1:count
-            switch sensors_position(i,2)
-                case 1 
-                    plot(room_grid(1,sensors_position(i,1)), room_grid(2,sensors_position(i,1)),'o','MarkerSize',9, 'MarkerEdgeColor',1/255*[247 176 240],'MarkerFaceColor',1/255*[247 176 240]);
-                case 2
-                    plot(room_grid(1,sensors_position(i,1))-25, room_grid(2,sensors_position(i,1)),'o','MarkerSize',9, 'MarkerEdgeColor',1/255*[247 176 240],'MarkerFaceColor',1/255*[247 176 240]);
-                    plot(room_grid(1,sensors_position(i,1))+25, room_grid(2,sensors_position(i,1)),'o','MarkerSize',9, 'MarkerEdgeColor',1/255*[247 176 240],'MarkerFaceColor',1/255*[247 176 240])            
-        
-            end
-        end
+        plot(sensors(:,2), sensors(:,3),'o','MarkerSize',9, 'MarkerEdgeColor',1/255*[247 176 240],'MarkerFaceColor',1/255*[247 176 240]);
         plot(room_grid(1,sensors_under_attack), room_grid(2,sensors_under_attack),'o','MarkerSize',9, 'MarkerEdgeColor',1/255*[255 0 0]);
-        plot(room_grid(1,estimated_attack), room_grid(2,estimated_attack),'*','MarkerSize',9, 'MarkerEdgeColor',1/255*[255 0 0]);
-        grid on
-        
+        plot(estimated_attack(:,1), estimated_attack(:,2),'*','MarkerSize',9, 'MarkerEdgeColor',1/255*[255 0 0]);
+        grid on,        
         %legend(plot([1,2,3,4,5]),{'Targets','Estimated targets','Sensors','Sensors under attack','Estimated attacks','Location','eastoutside'})
 
         xticks(100:100:1000)
@@ -91,24 +89,3 @@ function room(x_estimated,a_estimated,Tstart,Tmax)
     end
 
 end
-
-% clc
-% close all
-% clear all
-% load('localization.mat')
-% n = size(A,1);
-% q = size(D,1);
-% T = n+q;
-% O = [D, eye(q)];
-% for i=1:T
-%     O = [O ; D*A^i eye(q)];
-% end
-% 
-% %% Check the rank of O: O is not full rank...
-% size(O)
-% rank(O)
-% 
-% %% Graphical representation of the dynamic CPS
-
-
-
